9f2269b7881eae43c98837195ae6598dbe6d364b
Maan-Teckwani <145527836+Maan-Teckwani@users.noreply.github.com>
2026-01-21 21:33:36 +0530

    Update program 5.R

--- program 5.R
+++ program 5.R
@@ -15,4 +15,3 @@ cat("Area of rectangle with width 7 and height 3: ", area2, "\n")
 cat("Area of rectangle with width 0 and height 8: ", area3, "\n")
 cat("Area of rectangle with width 4.5 and height 6.2: ", area4, "\n")
 
-"Parizaa 23BCE0760"
